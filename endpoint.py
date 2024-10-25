832b27950a421a9af9387331cb935f5fcd1945ee
Tim Whelan <78320738+twhelan25@users.noreply.github.com>
2024-10-25 10:23:48 -0600

    Update endpoint.py

--- endpoint.py
+++ endpoint.py
@@ -1,5 +1,6 @@
 import socket
 import os
+
 host = '$ip'
 port = 8000
 
@@ -7,32 +8,30 @@ wordlist = "/usr/share/wordlists/dirb/common.txt"
 
 # set up request to endpoint
 def fuzz_endpoint(wordlist):
-  try:
-    # Open wordlist
-    with open(wordlist, 'r') as file:
-      for line in file:
-        # Clean up new lines
-        command = line.strip()
+    try:
+        # Open wordlist
+        with open(wordlist, 'r') as file:
+            for line in file:
+                # Clean up new lines
+                command = line.strip()
 
-        # Establish connection to the server
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-          s.connect((host, port))
+                # Establish connection to the server
+                with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                    s.connect((host, port))
 
-          # Send fuzzing to target
-          s.sendall(command.encode() + b'\n')
+                    # Send fuzzing to target
+                    s.sendall(command.encode() + b'\n')
 
-          # Recieve the response
-          response = s.recv(1024).decode().strip()
+                    # Receive the response
+                    response = s.recv(1024).decode().strip()
 
-          # Logic to catch valid response
-          if response != "" and "is not defined" not in response and "leading zeros" not in response:
-            print(f"Response {response}") 
+                    # Logic to catch valid response
+                    if response != "" and "is not defined" not in response and "leading zeros" not in response:
+                        print(f"Response {response}") 
 
-except FileNotFoundError:
-  print(f"File doesn't exist")
-except Exception as e:
-  print(f"Error has occured {e}")
+    except FileNotFoundError:
+        print(f"File doesn't exist")
+    except Exception as e:
+        print(f"Error has occurred {e}")
 
 fuzz_endpoint(wordlist)
-
-        
